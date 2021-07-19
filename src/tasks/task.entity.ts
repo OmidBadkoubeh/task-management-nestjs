ad717674866e575afb1b1fe70a0aedd9ddfaae76
Omid Badkoubeh <omid.badkoubeh@gmail.com>
2021-07-19 21:21:32 +0430

    added task entity class

---
+++ src/tasks/task.entity.ts
@@ -0,0 +1,15 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+import { TaskStatus } from './task.model';
+
+@Entity()
+export class Task extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: TaskStatus;
+}
