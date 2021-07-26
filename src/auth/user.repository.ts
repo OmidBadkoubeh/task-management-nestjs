cec741cc1a143c5588ba08965a66ca8acbf5fdeb
Omid Badkoubeh <omid.badkoubeh@gmail.com>
2021-07-26 21:42:31 +0430

    unique username validation

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,3 +1,7 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
@@ -11,6 +15,16 @@ export class UserRepository extends Repository<User> {
     const user = new User();
     user.username = username;
     user.password = password;
-    await user.save();
+
+    try {
+      await user.save();
+    } catch (error) {
+      if (error.code === '23505') {
+        // duplicate username
+        throw new ConflictException(`Username "${username}" already exists!`);
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
