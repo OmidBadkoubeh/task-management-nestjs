bc4ec21d81521964be7e72866dd11fdb87f06cb5
Omid Badkoubeh <omid.badkoubeh@gmail.com>
2021-07-29 12:03:13 +0430

    validate passwords

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,6 +11,13 @@ export class AuthController {
   async signUp(
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
   ): Promise<void> {
-    this.authService.signUp(authCredentialsDto);
+    return this.authService.signUp(authCredentialsDto);
+  }
+
+  @Post('/signin')
+  async signIn(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
+  ): Promise<void> {
+    return this.authService.signIn(authCredentialsDto);
   }
 }
